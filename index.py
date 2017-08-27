769090022e7e94acf48b8cf0f26dca0372a8d51a
Ghusar <ghusarhardikr@gmail.com>
2017-08-27 21:41:16 +0530

    login System implemented

---
+++ index.py
@@ -0,0 +1,28 @@
+try:
+    from urllib.parse import urlparse
+except ImportError:
+     from urlparse import urlparse
+
+from urllib2 import Request, urlopen
+
+import urllib
+
+import ssl
+
+import time
+
+url = 'https://10.100.56.55:8090/login.xml'
+
+post_fields = {
+                'mode':'191',
+                'username':'*****',
+                'password':'*****',
+                'a':'1503796951397',
+                'producttype':'0'
+            }     # Set POST fields here
+
+while True:
+    request = Request(url, urllib.urlencode(post_fields))
+    json = urlopen(request,context=ssl._create_unverified_context()).read().decode()
+    print json
+    time.sleep(3600)
